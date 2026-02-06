887f67ca27673b5b37808f065140845d700197e5
sreevedvp <sreevedvp@example.com>
2026-02-06 21:15:51 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'raw-lens',
+  webDir: 'www'
+};
+
+export default config;
